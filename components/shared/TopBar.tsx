9adc2e84ae7393d9bf65fd6b47352c8e08de203c
Hungnhatit <nhatpro204@gmail.com>
2024-10-12 10:31:20 +0700

    Update shared components

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,8 +1,12 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { dark } from '@clerk/themes'
 
 function TopBar() {
+  const isUserLoggedIn = true;
+
   return (
     <nav className='topbar'>
       <Link
@@ -17,6 +21,41 @@ function TopBar() {
         ></Image>
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
       </Link>
+
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <Image
+                  src="/assets/logout.svg"
+                  alt='logout'
+                  width={24}
+                  height={24}
+                ></Image>
+
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4 "
+            }
+          }}
+        >
+
+        </OrganizationSwitcher>
+
+      </div>
+
+
+
+
+
     </nav>
   )
 }
