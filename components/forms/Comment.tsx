694eedce3640556e5afe2e190c4d3337c5e17b83
Hungnhatit <nhatpro204@gmail.com>
2024-10-14 15:46:38 +0700

    Make comment component form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,48 +1,81 @@
+'use client'
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { usePathname, useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { CommentValidation } from "@/lib/validations/thread"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { z } from "zod"
+import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.action";
+
 interface Props {
   threadId: string,
   currentUserImg: string,
   currentUserId: string
 }
 
+
+
 const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: ""
+    }
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(threadId, values.thread, JSON.parse(currentUserId), pathname);
+    
+    form.reset();
+  }
+
+
   return (
     <>
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10 mt-10"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="comment-form"
         >
           <FormField
             control={form.control}
             name="thread"
             render={({ field }) => (
-              <FormItem className='w-full flex flex-col gap-3'>
-                <FormLabel className='text-base-semibold text-gray-200'>
-                  Content
+              <FormItem className='w-full flex items-center gap-3'>
+                <FormLabel>
+                  <Image
+                    src={currentUserImg}
+                    alt="Profile Imafe"
+                    width={48}
+                    height={48}
+                    className="rounded-full object-cover"
+                  ></Image>
                 </FormLabel>
 
-                <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
-                  <Textarea
-                    rows={15}
-
+                <FormControl className='border-none bg-transparent'>
+                  <Input
+                    type="text"
+                    placeholder="Comments..."
+                    className="no-focus text-light-1 outline-none"
                     {...field}
-                  ></Textarea>
+                  ></Input>
                 </FormControl>
-                <FormMessage />
               </FormItem>
             )}
 
-
-
-
           />
-
-
-
 
           <Button
             type="submit"
-            className="bg-primary-500"
-          >Post Thread</Button>
+            className="comment-form_btn"
+          >Reply</Button>
         </form>
 
 
